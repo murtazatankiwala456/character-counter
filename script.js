f0e3e23d276e3bccd1a01494e6ce6c333b7bb4a9
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-03-25 12:31:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector("#textArea");
+const text = document.querySelector(".text");
+const maxChars = 240;
+
+function updateCharacterCount() {
+  let inputText = textarea.value.slice(0, maxChars); // Limit input to maxChars length
+  textarea.value = inputText; // Update the textarea value
+
+  const charCount = inputText.length;
+  const charsLeft = maxChars - charCount;
+
+  text.innerHTML = `<p>You have written <span class='text-success'>${charCount}</span> words and <span class='text-danger'>${charsLeft}</span> characters remaining</p>`;
+}
+
+textarea.addEventListener("input", updateCharacterCount);
